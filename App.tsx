cbefb07f424b70d437bca609eb413dbbf56c9e23
hmncube <happinessmncube@gmail.com>
2022-10-05 15:24:50 +0200

    Initial configure redux

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import myStore from './myStore';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! </Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={myStore}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app! </Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
---
+++ myStore.ts
@@ -0,0 +1,13 @@
+import { applyMiddleware, createStore, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import rootReducer from './src/reducers';
+
+let logger = createLogger({
+  timestamps: true,
+  duration: true,
+});
+
+const tilteStore = createStore(rootReducer, compose(applyMiddleware(thunk, logger)));
+
+export default tilteStore;
